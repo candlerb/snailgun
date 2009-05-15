5d1ddac1256b0c88525bbbcf084462bd033eaac5
Brian Candler <b.candler@pobox.com>
2009-05-15 15:31:30 +0100

    Set process name

--- lib/snailgun/server.rb
+++ lib/snailgun/server.rb
@@ -73,11 +73,14 @@ module Snailgun
 
       ARGV.replace(args)
       if !e.empty?
+        $0.replace('-e')
         e.each { |expr| eval(expr, TOPLEVEL_BINDING) }
       elsif ARGV.empty?
+        $0.replace('-')
         eval(STDIN.read, TOPLEVEL_BINDING)
       else
         cmd = ARGV.shift
+        $0.replace(cmd)
         load(cmd)
       end
     end
